60312da71f0195d407bc7f42848cdb8da19e3bec
satire1123 <yyaanggg@gmail.com>
2026-07-31 09:49:58 +0800

    changed columns, added head, plot

--- notebooks/visualize.py
+++ notebooks/visualize.py
@@ -1,3 +1,17 @@
+import matplotlib.pyplot as plt
+from pathlib import Path
 import pandas as pd
-demand_params = pd.read_csv("data/demand_params.scv")
-print(demand_params)
+
+
+def main():
+    base_dir = Path(__file__).resolve().parent.parent
+    data_path = base_dir / "data" / "demand_params.csv"
+    df = pd.read_csv(data_path)
+    df = df.rename(columns={"demand point": "demand_point","j": "index","Pj": "population", "rj": "poverty_rate", 
+                       "Vj": "vulnerable", "j.1": "congestion"})
+    df.head()
+    df.plot
+    plt.show
+
+if __name__ == "__main__":
+    main()
